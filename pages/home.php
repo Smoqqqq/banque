ec38cd1b537cfb54c315a76df827544f461d94b5
alolilu <33064126+alolilu@users.noreply.github.com>
2021-10-01 14:03:10 +0200

    Update home.php

--- pages/home.php
+++ pages/home.php
@@ -1,1 +1,11 @@
-<h1>TEST</h1>
+<div class="header">
+        <div class="row">
+            <div class="col-6 header-right">
+                <img src="IMG/logo.png" alt=""><p><h3>Fortuna</h3>&nbsp;&nbsp;<h3>Banque</h3></p>
+            </div>
+            <div class="col-6 header-left">
+                
+            </div>
+        </div>
+    </div>
+
