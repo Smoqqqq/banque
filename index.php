05f47c33a50d68c714c78c497f60094b528e1d84
alolilu <33064126+alolilu@users.noreply.github.com>
2021-10-11 11:12:54 +0200

    STYLES

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 
 <!DOCTYPE html>
 <html lang="fr">
-
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0" />
--- modules/navbar.php
+++ modules/navbar.php
@@ -1,7 +1,7 @@
 <div class="header">
         <div class="row">
             <div class="col-4 header-right">
-                <img src="IMG/logo.png" alt=""><p><h3><e>Fortuna</e><br>Banque</h3></p>
+                <img src="assets/img/logo.png" alt=""><p><h3><e>Fortuna</e><br>Banque</h3></p>
             </div>
             <div class="col-4  header-middle">
                 <h4>Créer un compte un gagner de l'argents</h4>
