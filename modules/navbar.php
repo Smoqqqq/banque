6a4bbb9d8275b4d27b38e82145605edab48bb2c0
alolilu <33064126+alolilu@users.noreply.github.com>
2021-10-11 16:01:49 +0200

    page d'accueil finis

--- modules/navbar.php
+++ modules/navbar.php
@@ -1,13 +1,14 @@
 <div class="header">
         <div class="row">
-            <div class="col-4 header-right">
-                <img src="assets/img/logo.png" alt=""><p><h3><e>Fortuna</e><br>Banque</h3></p>
+            <div class="col-4 header-left">
+            <a href="<?=$routes['home']?>"><img src="assets/img/logo.png" alt=""><p><h3><e>Fortuna</e><br>Banque</h3></p></a>
             </div>
             <div class="col-4  header-middle">
                 <h4>Créer un compte un gagner de l'argents</h4>
             </div>
-            <div class="col-4  header-left">
+            <div class="col-4  header-right">
                 <button>Déja client ?</button>
+                <a href="<?=$routes['inscription']?>">Inscription</a>
             </div>
         </div>
     </div>
